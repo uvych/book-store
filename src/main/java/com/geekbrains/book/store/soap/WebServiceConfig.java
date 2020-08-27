1977a9ac0561c4a8da93568c5ad6b77d5224e0e1
Georgy <georgy143w@gmail.com>
2020-08-27 22:05:03 +0300

    Added SOAP

---
+++ src/main/java/com/geekbrains/book/store/soap/WebServiceConfig.java
@@ -0,0 +1,42 @@
+package com.geekbrains.book.store.soap;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext){
+        MessageDispatcherServlet servlet=new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet,"/ws/*");
+    }
+
+    @Bean(name = "books")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema booksSchema){
+        DefaultWsdl11Definition defaultWsdl11Definition=new DefaultWsdl11Definition();
+        defaultWsdl11Definition.setPortTypeName("BooksPort");
+        defaultWsdl11Definition.setLocationUri("/ws");
+        defaultWsdl11Definition.setTargetNamespace("http://some.com/ws/bookstore");
+        defaultWsdl11Definition.setSchema(booksSchema);
+        return defaultWsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema booksSchema(){
+        return new SimpleXsdSchema(new ClassPathResource("books.xsd"));
+    }
+}
